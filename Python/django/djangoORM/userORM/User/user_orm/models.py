0aac6678bf0cb144de590e0802451c53f70784f0
whylock <shawnlockhart7@gmail.com>
2020-08-13 16:02:20 -0500

    Users Assignment

---
+++ Python/django/djangoORM/userORM/User/user_orm/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=254)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"<User object: {self.first} ({self.id})>"
---
+++ Python/django/djangoORM/userORM/User/user_orm/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+# Create your views here.
+
+
+def root(request):
+    all_users = User.objects.all()
+
+    context = {
+        'user': all_users
+    }
+    return render(request, 'index.html', context)
+
+
+def adduser(request):
+    User.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email_address=request.POST['email'],
+        age=request.POST['age'])
+    return redirect('/')
