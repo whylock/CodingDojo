ae92aed2e396c120620fc7e7e8c6d204e9fe5dd4
whylock <shawnlockhart7@gmail.com>
2020-08-31 19:12:35 -0500

    propitup assignment

--- mern/intro_to_react/first_test/src/App.js
+++ mern/intro_to_react/first_test/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import PropItUp from "./components/propitup";
+
 function App() {
 	return (
 		<div className="App">
@@ -13,6 +15,11 @@ function App() {
 				<li>Run a marathon</li>
 				<li>feed the dogs</li>
 			</ul>
+
+			<PropItUp firstName="Jane" lastName="Doe" age="45" hairColor="Black" />
+			<PropItUp firstName="John" lastName="Smith" age="88" hairColor="Brown" />
+			<PropItUp firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown" />
+			<PropItUp firstName="Maria" lastName="Smith" age="62" hairColor="Brown" />
 		</div>
 	);
 }
