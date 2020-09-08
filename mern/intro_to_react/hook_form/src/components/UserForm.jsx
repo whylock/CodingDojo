d3c9fb6b4a21ee81d78ac9e01b58410b3bb9eb44
whylock <shawnlockhart7@gmail.com>
2020-09-07 23:16:43 -0500

    more forms assignment

--- mern/intro_to_react/hook_form/src/components/UserForm.jsx
+++ mern/intro_to_react/hook_form/src/components/UserForm.jsx
@@ -1,40 +1,73 @@
 import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Feedback from "react-bootstrap/esm/Feedback";
 
 const UserForm = (props) => {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const [formState, setFormState] = useState({
+		username: "",
+		email: "",
+		password: "",
+		confirm_password: "",
+	});
 
-	const createUser = (e) => {
-		e.preventDefault();
-		const newUser = { username, email, password };
+	const onChangeHandler = (e) => {
+		setFormState({
+			...formState,
+			[e.target.name]: e.target.value,
+		});
 	};
 
+	const addUser = (e) => {
+		e.preventDefault();
+		setFormState({
+			username: "",
+			email: "",
+			password: "",
+			confirm_password: "",
+		});
+	};
+
+	const [validated, setValidated] = useState(false);
+	const basicValid = (input) => {
+		if (input.length <= 2) {
+			return false;
+		}
+		return true;
+	};
 	return (
 		<Container>
-			<Form>
+			<Form noValidate validated={validated}>
 				<Form.Group controlId="formUsername">
 					<Form.Label>Username</Form.Label>
-					<Form.Control type="text" onChange={(e) => setUsername(e.target.value)} placeholder="Enter a username" />
+					<Form.Control type="text" name="username" id="username" onChange={onChangeHandler} placeholder="Enter a username" />
+					{basicValid(formState.username) ? "" : <p className="ml-2 text-danger">Username should be more than 2 character</p>}
 				</Form.Group>
 				<Form.Group controlId="formEmail">
 					<Form.Label>Email Address</Form.Label>
-					<Form.Control type="email" onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" />
+					<Form.Control type="email" name="email" onChange={onChangeHandler} required placeholder="name@example.com" />
+					{basicValid(formState.email) ? "" : <p className="ml-2 text-danger">Email should be more than 2 character</p>}
 				</Form.Group>
 				<Form.Group controlId="formPassword">
 					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+					<Form.Control type="password" name="password" onChange={onChangeHandler} required placeholder="Password" />
+					{basicValid(formState.password) ? "" : <p className="ml-2 text-danger">Password should be more than 2 character</p>}
+				</Form.Group>
+				<Form.Group controlId="formPasswordConfrom">
+					<Form.Label>Confirm Password</Form.Label>
+					<Form.Control type="password" name="confirm_password" onChange={onChangeHandler} required placeholder="Password" />
 				</Form.Group>
 				<Button variant="outline-dark" type="submit">
 					Create User
 				</Button>
 			</Form>
 			<div>
-				<p>{username}</p>
-				<p>{email}</p>
+				<p>{formState.username}</p>
+				<p>{formState.email}</p>
+				<p>{formState.password}</p>
+				<p>{formState.confirm_password}</p>
 			</div>
 		</Container>
 	);
