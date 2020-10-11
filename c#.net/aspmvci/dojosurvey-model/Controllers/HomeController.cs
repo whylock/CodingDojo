c6aabb37a7c1d18fe20e86710793388e4e33402f
whylock <shawnlockhart7@gmail.com>
2020-10-11 15:13:11 -0500

    assignment

---
+++ c#.net/aspmvci/dojosurvey-model/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dojosurvey_model.Models;
+
+namespace dojosurvey_model.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Result")]
+        public IActionResult Result(Survey survey)
+        {
+            return View(survey);
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ c#.net/aspmvci/dojosurvey-model/Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace dojosurvey_model.Models
+{
+    public class Survey
+    {
+        public string name { get; set; }
+        public string location { get; set; }
+        public string language { get; set; }
+        public string textarea { get; set; }
+    }
+}
