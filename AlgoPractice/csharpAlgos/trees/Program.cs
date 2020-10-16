c3635f2722c097004894be67fc2fc670d0d3f73f
whylock <shawnlockhart7@gmail.com>
2020-10-16 16:31:36 -0500

    BST

--- AlgoPractice/csharpAlgos/trees/Program.cs
+++ AlgoPractice/csharpAlgos/trees/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
 namespace trees
 {
@@ -8,35 +9,37 @@ namespace trees
         {
             BST tree = new BST();
             tree.Insert(6);
-            tree.Insert(7);
+            tree.Insert(4);
             tree.Insert(8);
-            tree.Insert(5);
-            tree.Insert(2);
+            tree.Insert(7);
             tree.Insert(9);
-            tree.Insert(10);
-            // Console.WriteLine("**************");
-            // tree.Min();
-            // tree.Max();
-            // Console.WriteLine("**************");
+            // tree.Find(10);
+            // tree.Insert(5);
+            // tree.Insert(3);
+            // tree.Insert(10);
+            // Console.WriteLine(tree.Depth(tree.root));
+            // Console.WriteLine(tree.IsBalanced());
+            Console.WriteLine(tree.IsFull(tree.root));
         }
 
         public class BNode
         {
-            public int value {get;set;}
-            public BNode left {get;set;}
-            public BNode right {get;set;}
+            public int value { get; set; }
+            public BNode left { get; set; }
+            public BNode right { get; set; }
             public BNode(int v)
             {
                 value = v;
                 left = null;
                 right = null;
             }
+            
         }
 
         public class BST
         {
             //Setup
-            public BNode root {get;set;}
+            public BNode root { get; set; }
             public BST()
             {
                 root = null;
@@ -45,40 +48,36 @@ namespace trees
             public string Insert(int val)
             {
                 BNode node = new BNode(val);
-                if(root == null)
+                if (root == null)
                 {
                     root = node;
-                    Console.WriteLine($"{node.value} is now the root.");
                     return $"{node.value} is now the root.";
                 }
                 else
                 {
                     BNode current = root;
-                    while(true)
+                    while (true)
                     {
                         if (val == current.value)
                         {
-                            Console.WriteLine("Root and the value are the same");
                             return "Root and the value are the same";
                         }
                         //Less Than
-                        if(val < current.value)
+                        if (val < current.value)
                         {
-                            if(current.left == null)
+                            if (current.left == null)
                             {
                                 current.left = node;
-                                Console.WriteLine(current.value);
                                 return $"Current: {current.left.value}";
                             }
                             current = current.left;
                         }
-                        else 
+                        else
                         // Greater Than
                         {
-                            if(current.right == null)
+                            if (current.right == null)
                             {
                                 current.right = node;
-                                Console.WriteLine(current.value);
                                 return $"Current: {current.right.value}";
                             }
                             current = current.right;
@@ -90,14 +89,14 @@ namespace trees
 
             public BNode Min()
             {
-                if(root == null)
+                if (root == null)
                 {
                     return null;
                 }
                 else
                 {
                     BNode current = root;
-                    while(current.left != null)
+                    while (current.left != null)
                     {
                         current = current.left;
                     }
@@ -106,18 +105,78 @@ namespace trees
             }
             public BNode Max()
             {
-                if(root == null)
+                if (root == null)
                 {
                     return null;
                 }
                 else
                 {
                     BNode current = root;
-                    while(current.right != null)
+                    while (current.right != null)
                     {
                         current = current.right;
                     }
                     return current;
+                }
+            }
+
+            // BST Height (give me the height of a given BST)
+            public int Depth(BNode root)
+            {
+                if (root == null) return 0; 
+                int LeftDepth = Depth(root.left);
+                int RightDepth = Depth(root.right);
+                //            condition       ?   truthy        :    falsey
+                return RightDepth > LeftDepth ? RightDepth + 1  : LeftDepth + 1;
+            }
+
+
+            // BST IsBalanced (Tell me if a tree is balanced by height of left and right being no greater than 1 in difference)
+
+            public bool IsBalanced()
+            {
+
+                Console.WriteLine(Math.Abs(Depth(root.left) - Depth(root.right)));
+
+
+                return (Math.Abs(Depth(root.left) - Depth(root.right)) > 1) ? false : true;
+            }
+
+            public bool IsFull(BNode root)
+            {
+                if(root == null) return true;
+                if(root.left == null && root.right == null) return true;
+                if(root.left != null && root.right != null) return (IsFull(root.left) && IsFull(root.right));
+                return false; // base case
+            }
+
+            // Tomorrow’s algo: Remove a given node from a BST
+            // Think about your potential cases:
+            // Case 1) the node to remove is at root
+            // Case 2) the node to remove has no children
+            // Case 3) the node to remove has 1 child
+            // Case 4) the node to remove has 2 children
+            // Case 5) the node doesn't exist in the tree
+
+            // Maintain the integrity of the tree, meaning when you go through it later the values should still be in their proper left/right placement from the node above it
+
+            public BNode Remove(BNode root, int NodeValue)
+            {
+                if(root == null) return null;
+                if(NodeValue < root.value)
+                {
+                    root.left = Remove(root.left, NodeValue);
+                }
+                else if (NodeValue > root.value)
+                {
+                    root.right = Remove(root.right, NodeValue);
+                }
+                else 
+                {
+                    if(root.left == null)
+                    {
+                        return root.right;
+                    }
                 }
             }
 
